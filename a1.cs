7f8cbb4be271a4d810cd7450998cae64b5edd141
“biyc <1040800070@qq.com>
2022-05-31 13:00:44 +0800

    []

--- a1.cs
+++ a1.cs
@@ -4,11 +4,5 @@
     {
         private string a1=1;
         public string aa ;
-        
-        private string a1=1;
-        private string a2;
-        private string a1=1;
-        public string aa ;
-        
     }
 }
