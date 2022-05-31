611ce2a06c43764c0104787cb6d409f346a87a87
“biyc <1040800070@qq.com>
2022-05-31 11:58:38 +0800

    a

--- a1.cs
+++ a1.cs
@@ -2,7 +2,13 @@
 {
     public class a1
     {
-        private string a1;
+        private string a1=1;
+        public string aa ;
+        
+        private string a1=1;
         private string a2;
+        private string a1=1;
+        public string aa ;
+        
     }
 }
